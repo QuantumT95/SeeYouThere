ef558ee365d59219eb31de5de7b1e8d34aef74b2
QuantumT95 <tran.charlie@ymail.com>
2017-08-01 20:46:22 -0500

    added current location on window load function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,24 +4,36 @@
 // async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
  
 
-// apiKey: "AIzaSyARBt0KpGyGMoEle_MskA5Xz56lPvOJE7g"
-
-var location1;
-var res1;
-
-$(".submit").on("click", function() {
-	preventDefault();
-
-	var location1 = $("#address_1").val();
-	var res1 = location1.toString().replace(/ /g,"+");
+// apiKey: "AIzaSyB45u6q4Ep9T99aYb1tEo2NmkJ-vxJD8pk"
 
 
-	console.log(location1);
-	console.log(res1);
+// Geolocation url for finding current location
+src="https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB45u6q4Ep9T99aYb1tEo2NmkJ-vxJD8pk"
 
-	
 
-});
+
+
+// window.onload = function() {
+//   var startPos;
+//   var geoSuccess = function(position) {
+//     startPos = position;
+//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+//   };
+//   navigator.geolocation.getCurrentPosition(geoSuccess);
+//   console.log(startPos);
+// };
+
+getCurrentPosition();
+
+
+// $(".location").on("click", function() {
+// 	preventDefault();
+
+
+// })
+
+
 
 
 // https://maps.googleapis.com/maps/api/js?key=AIzaSyARBt0KpGyGMoEle_MskA5Xz56lPvOJE7g&callback=initMap
