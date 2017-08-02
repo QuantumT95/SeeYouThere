a0d7472d4430a1f7a8cd2817f51e6f046021e5af
QuantumT95 <tran.charlie@ymail.com>
2017-08-01 21:04:58 -0500

    added on load function to find current location; printed long and lat onto page. Will need to store that information somewhere

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,21 +10,26 @@
 // Geolocation url for finding current location
 src="https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB45u6q4Ep9T99aYb1tEo2NmkJ-vxJD8pk"
 
+// Google Maps API v3 - create google maps on fly and add our current position to it
+src="//maps.googleapis.com/maps/api/js?v=3.exp&sensor=true"
 
 
 
-// window.onload = function() {
-//   var startPos;
-//   var geoSuccess = function(position) {
-//     startPos = position;
-//     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-//     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-//   };
-//   navigator.geolocation.getCurrentPosition(geoSuccess);
-//   console.log(startPos);
-// };
 
-getCurrentPosition();
+
+window.onload = function() {
+  var startPos;
+  var geoSuccess = function(position) {
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  };
+  navigator.geolocation.getCurrentPosition(geoSuccess);
+};
+
+
+
+
 
 
 // $(".location").on("click", function() {
