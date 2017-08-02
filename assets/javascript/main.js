66e0b7b2b9b3d5520c4720982a02d43cdc3fe09c
QuantumT95 <tran.charlie@ymail.com>
2017-08-01 19:31:31 -0500

    added on click function for getting address 1 text box and turn it into a var

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,27 @@
 
 
 // Google maps API
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-  type="text/javascript"> </script>
+// async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+ 
 
-apiKey: "AIzaSyARBt0KpGyGMoEle_MskA5Xz56lPvOJE7g"
+// apiKey: "AIzaSyARBt0KpGyGMoEle_MskA5Xz56lPvOJE7g"
+
+var location1;
+var res1;
+
+$(".submit").on("click", function() {
+	preventDefault();
+
+	var location1 = $("#address_1").val();
+	var res1 = location1.toString().replace(/ /g,"+");
 
 
+	console.log(location1);
+	console.log(res1);
+
+	
+
+});
+
+
+// https://maps.googleapis.com/maps/api/js?key=AIzaSyARBt0KpGyGMoEle_MskA5Xz56lPvOJE7g&callback=initMap
